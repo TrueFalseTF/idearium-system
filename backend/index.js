dba28dae93dff1bb775b39f0533e460ed583d424
TrueFalseTF <dimon_mcensk@mail.ru>
2019-04-18 00:22:10 +0300

    Вот теперь написан сервер

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,7 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-app.listen(port, function());
+app.listen(port, function() {
+    console.log ("the server opened. the port: " + port);
+});
 
