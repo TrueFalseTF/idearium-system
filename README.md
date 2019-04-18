8d6be9b2b00ece5b404d141f136ef342ad8b16a7
TrueFalseTF <dimon_mcensk@mail.ru>
2019-04-18 20:02:27 +0300

    Создана ветка работы с фронтенд

--- README.md
+++
@@ -1,1 +0,0 @@
-# ideator-system
--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-var port = process.env.PORT || 5000;
-
-app.listen(port, function() {
-    console.log ("the server opened. the port: " + port);
-});
-
