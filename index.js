598f1648c3d70786ee863895e52562b60cba0575
TrueFalseTF <dimon_mcensk@mail.ru>
2019-04-18 23:44:01 +0300

    Реализован вывод статической страницы из папки client при обращении к серверу

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require("express");
-var app = express();
+var path = require("path");
 
 var port = process.env.PORT || 5000;
+var clientPath = path.join(__dirname, "client");
+
+var app = express();
+app.use(express.static(clientPath));
 
 app.listen(port, function() {
     console.log ("the server opened. the port: " + port);
